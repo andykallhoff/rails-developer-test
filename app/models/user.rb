2596056a375f6d8dbf2764f100ba42eb82ef0804
Andy Kallhoff <akallhoff@verisys.com>
2018-05-02 11:16:50 -0600

    adds role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_default_role
+
+  ROLES = %w(admin editor vanilla guest).freeze
+
+  def admin?
+    role == "admin"
+  end
+
+  def editor?
+    role == "editor"
+  end
+
+  def vanilla?
+    role == "vanilla"
+  end
+
+  private
+    def set_default_role
+      self.role ||= "guest"
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#--- Users
+User.destroy_all
+roles = %w(guest vanilla editor admin)
+roles.each do |role|
+  User.create(email: "#{role}@test.com", password: "thisisapass", password_confirmation: "thisisapass", role: role)
+end
+
+#--- Articles
+#Article.destroy_all
